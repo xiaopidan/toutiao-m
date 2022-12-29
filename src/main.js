b4f0aed6343ac4c31bb676f0fc39efb2a317fc8c
liangyinzhong <853147897@qq.com>
2022-12-29 15:02:54 +0800

    引用iconfont 与 vant

--- src/main.js
+++ src/main.js
@@ -3,7 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
 import './styles/index.less'
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
